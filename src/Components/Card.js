ada268fb9d332560051e090b977f36ef1f7f677c
kaverndsp <kaverndsp@users.noreply.github.com>
2020-01-23 17:49:42 -0600

    adding more styles

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -9,7 +9,7 @@ import "./Card.css";
 
 function Card() {
   const [info, getInfo] = useState([]);
-
+  
   useEffect(() => {
     axios
       .get("https://api.nasa.gov/planetary/apod?api_key=ifkKjIejleSl0G7vTSqEa1MW9TVgfhmjn5RhzfOW")
--- src/Components/CardInfo.js
+++ src/Components/CardInfo.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Card.css";
 import styled from 'styled-components'
-import { Jumbotron, Button, Container } from 'reactstrap';
+import { Jumbotron, Container, Card, CardBody, CardTitle, CardText, CardImg } from 'reactstrap';
 
 const BigImage = styled.img`
-max-width:1280px;
+max-width:1000px;
 max-height:100%;
 margin-bottom:3%;
+&:hover{
+    max-width:100%;
+   max-height:100%;
+}
 
 `;
 
@@ -22,8 +26,19 @@ const TwoDiv = styled.div`
 
 `;
 
+const Para = styled.p`
+`;
 
+const HeaderOne = styled.h1`
+text-shadow:10px 4px 30px #000000;
+margin-bottom:2%;
+`;
 
+const Border = styled.p`
+border-top: 1.5px solid lightgrey;
+margin-bottom: 4rem;
+
+`;
 
 function CardInfo (props){
    console.log(props)
@@ -33,21 +48,32 @@ function CardInfo (props){
          <Jumbotron fluid>
              <Container fluid>
                  <TwoDiv>
-        <h1 className="display-2">NASA PHOTO OF THE DAY</h1>
-        <p className="display-4">{props.data.title}</p>
+        <HeaderOne className="display-2">NASA PHOTO OF THE DAY</HeaderOne>
+        <Border className="display-4">{props.data.title}</Border>
         </TwoDiv>
         <NewDiv>
+            
             <h5>Photo taken by: {props.data.copyright}</h5>
-            <h5>Date:{props.data.date}</h5>
+            <h5>Date: {props.data.date}</h5>
             
         </NewDiv>
-
+        {/* <Card>
+        <CardBody>
+          
+          
+         
+        </CardBody>
+        <CardImg bottom width="100%" src={props.data.url} alt="Card image cap" /> 
+        <CardText>
+    <small className="text-muted">Last updated {props.data.date}</small>
+          </CardText>
+      </Card> */}
         
         <BigImage src={props.data.hdurl}/>
         <hr className="my-2" />
         <p>{props.data.explanation}</p>
         <p className="lead">
-          <Button color="primary">Learn More</Button>
+          {/* <Button color="primary">Learn More</Button> */}
         </p>
         </Container>
       </Jumbotron>
